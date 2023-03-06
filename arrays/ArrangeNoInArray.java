b4ac6cb2aa1ca5e480c3274c3a723bf99d7b7320
gBhatt1998 <gauravbhatt1998@gmail.com>
2023-03-06 15:30:46 +0530

    updated

--- arrays/ArrangeNoInArray.java
+++ arrays/ArrangeNoInArray.java
@@ -2,7 +2,7 @@ package arrays;
 
 import java.util.Scanner;
 
-/*Arrange Numbers in Array
+/*Arrange Numbers in Array test
 
 You have been given an empty array(ARR) and its size N. The only input taken from the user will be N and you need not worry about the array.
 Your task is to populate the array using the integer values in the range 1 to N(both inclusive) in the order - 1,3,.......4,2.
